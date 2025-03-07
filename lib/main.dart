99ae181aa64b1bb5ae76e88be5baee182fa457fc
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2025-03-07 15:31:30 +0530

    removed notification feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:blue_sky/screens/home_screen.dart';
-import 'package:blue_sky/services/background_notification.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:workmanager/workmanager.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize workmanager for background tasks
-
-  await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
-
-  // Register a  periodic background task  to fetch weather updates evry 8 hours
-  await Workmanager().registerPeriodicTask(
-    "weatherTask",
-    "fetchWeather",
-    frequency: const Duration(minutes: 15),
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
